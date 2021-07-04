beb4f8734932bdc3bac3aee35242d55eecc48cfc
Spiritshinobi <85051279+Spiritshinobi@users.noreply.github.com>
2021-07-04 01:25:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1481,7 +1481,7 @@ case '.gay':
 					
 					
 					
-					case '(.testebot)':
+					case '(.testebotrpg2)':
                     memein = await kagApi.memeindo()
                     buffer = await getBuffer(`https://i.ibb.co/Gx8jtGM/IMG-20210529-WA0177.jpg`)
                     client.sendMessage(from, buffer, image, {quoted: mek, caption: 'baita teste'})
